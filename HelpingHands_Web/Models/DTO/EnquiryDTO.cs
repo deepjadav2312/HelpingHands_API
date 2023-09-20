cd19b57230cbf2d20bbacf95a51f03d13337ec3f
deepjadav2312 <deepjadav2312@gmail.com>
2023-09-20 11:00:28 +0530

    Add project files.

---
+++ HelpingHands_Web/Models/DTO/EnquiryDTO.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace HelpingHands_Web.Models.DTO
+{
+    public class EnquiryDTO
+    {
+       [Required]
+        public int Id { get; set; }
+
+        public int CompanyID { get; set; }
+        [ValidateNever]
+        public CompanyDTO Company { get; set; }
+
+
+        public string ApplicationUserId { get; set; }
+        [ValidateNever]
+        public ApplicationUserDTO ApplicationUser { get; set; }
+
+
+        [Required]
+        [DisplayName("User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DisplayName("User Email")]
+		[StringLength(20)]
+		[DataType(DataType.EmailAddress)]
+		[RegularExpression(@"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}", ErrorMessage = "Invalid Email Address")]
+		public string Email { get; set; }
+        [Required]
+        [DisplayName("User Phone Number")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "The phone number must be exactly 10 digits.")]
+        public int PhoneNumber { get; set; }
+        [Required]
+        [DisplayName("Enquiry Title")]
+        public string Title { get; set; }
+        [Required]
+        [DisplayName("Brif Description Of Your Enquiry")]
+        public string Description { get; set; }
+
+        public bool IsActive { get; set; }
+
+
+    }
+}
---
+++ HelpingHands_Web/Models/DTO/RegisterationRequestDTO.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HelpingHands_Web.Models.DTO
+{
+    public class RegisterationRequestDTO
+    {
+        [Required]
+        [RegularExpression(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", ErrorMessage = "Invalid Password Enter 8 character password like 'Example@123' ")]
+        public string Password { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? PostalCode { get; set; }
+        [Required]
+        [StringLength(13, MinimumLength = 9, ErrorMessage = "The field must be between 9 and 13 characters.")]
+        public string PhoneNumber { get; set; }
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
+        public string FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string? LastName { get; set; }
+        public string? Address { get; set; }
+        public string? Country { get; set; }
+        [Required]
+        [StringLength(50)]
+        [RegularExpression(@"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}", ErrorMessage = "Invalid Email Address Enter")]
+        public string Email { get; set; }
+    }
+}
